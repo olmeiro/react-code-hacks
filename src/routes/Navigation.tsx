43d44ac5c96b70c6bb0689303d1df46a41280d4b
Olmeiro orozco <olme.orozco@gmail.com>
2022-10-26 09:19:40 -0500

    feat: lazyload react-router-v6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,46 +6,44 @@ import { routes } from "./routes";
 
 import logo from "../assets/logo.svg";
 
-
-
 export const Navigation = () => {
   return (
-   <Suspense fallback={<span>Loading...</span>} >
-     <BrowserRouter>
-       <div className="main-layout">
-         <nav>
-           <img src={logo} alt="React Logo" />
-           <ul>
-             {routes.map(({to, name}) => {
-               return (
-                 <li key={to}>
-                   <NavLink
-                     to={to}
-                     className={({ isActive }) => (isActive ? "nav-active" : "")}
-                   >
-                     {name}
-                   </NavLink>
-                 </li>
-               );
-             })}
-           </ul>
-         </nav>
-    
-         <Routes>
-           {routes.map((route) => {
-             return (
-               <Route
-                 key={route.to}
-                 path={route.path}
-                 element={<route.Component />}
-               />
-             );
-           })}
-    
-           <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-         </Routes>
-       </div>
-     </BrowserRouter>
-   </Suspense>
+    <Suspense fallback={<span>Loading...</span>} >
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map(({ to, name }) => {
+                return (
+                  <li key={to}>
+                    <NavLink
+                      to={to}
+                      className={({ isActive }) => (isActive ? "nav-active" : "")}
+                    >
+                      {name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+
+          <Routes>
+            {routes.map((route) => {
+              return (
+                <Route
+                  key={route.to}
+                  path={route.path}
+                  element={<route.Component />}
+                />
+              );
+            })}
+
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
 import { lazy, LazyExoticComponent } from "react";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 type JSXComponent = () => JSX.Element;
 
 interface Route {
-  to: string,
-  path: string,
-  Component: LazyExoticComponent<JSXComponent> | JSXComponent,
-  name: string
+  to: string;
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
 }
 
 // implementando load lazy
@@ -16,27 +16,27 @@ interface Route {
 // const Lazy2 = lazy(()=> import(/*webpackChunckName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'))  // estos comp deben tener una export por default
 // const Lazy3 = lazy(()=> import(/*webpackChunckName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'))  // estos comp deben tener una export por default
 
-const Lazy1 = lazy(()=> import('../01-lazyload/pages/LazyPage1'))  // estos comp deben tener una export por default
-const Lazy2 = lazy(()=> import('../01-lazyload/pages/LazyPage2'))  // estos comp deben tener una export por default
-const Lazy3 = lazy(()=> import('../01-lazyload/pages/LazyPage3'))  // estos comp deben tener una export por default
+const Lazy1 = lazy(() => import("../01-lazyload/pages/LazyPage1")); // estos comp deben tener una export por default
+const Lazy2 = lazy(() => import("../01-lazyload/pages/LazyPage2")); // estos comp deben tener una export por default
+const Lazy3 = lazy(() => import("../01-lazyload/pages/LazyPage3")); // estos comp deben tener una export por default
 
 export const routes: Route[] = [
   {
-    to: '/lazy1',
-    path:'lazy1',
+    to: "/lazy1",
+    path: "lazy1",
     Component: Lazy1,
-    name: 'Lazy-1'
+    name: "Lazy-1",
   },
   {
-    to: '/lazy2',
-    path:'lazy2',
+    to: "/lazy2",
+    path: "lazy2",
     Component: Lazy2,
-    name: 'Lazy-2'
+    name: "Lazy-2",
   },
   {
-    to: '/lazy3',
-    path:'lazy3',
+    to: "/lazy3",
+    path: "lazy3",
     Component: Lazy3,
-    name: 'Lazy-3'
+    name: "Lazy-3",
   },
-]
+];

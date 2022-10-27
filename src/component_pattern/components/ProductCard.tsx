b5b2cf127cdf8739aa206829e2771b6389c065b8
Olmeiro orozco <olme.orozco@gmail.com>
2022-10-27 10:49:36 -0500

    feat: traditional component in React

--- src/component_pattern/components/ProductCard.tsx
+++ src/component_pattern/components/ProductCard.tsx
@@ -3,13 +3,22 @@ import styles from '../styles/styles.module.css'
 import { useProduct } from '../hooks/useProduct';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
+interface Props {
+  product: Product
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string
+}
+
+export const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy } = useProduct()
   return (
     <div className={styles.productCard} >
-      <img className={styles.productImg} src="./coffee-mug.png" alt="coffee mug" />
-      {/* <img className={styles.productImg} src={noImage} alt="no image" /> */}
-      <span className={styles.productDescription}>Coffe mug</span>
+      <img className={styles.productImg} src={product.img ? product.img : noImage} alt="coffee mug" />
+      <span className={styles.productDescription}>{product.title}</span>
 
       <div className={styles.buttonsContainer}>
         <button
@@ -26,3 +35,5 @@ export const ProductCard = () => {
     </div>
   )
 }
+
+// Esta es la manera tradicional de hacer componentes, vemos que tiene muchos styles, además es complicado informarle al padre sobre cambios, además como desarrolladores no tienen control al respecto, solo mandan el producto. Si tenemos una tarjeta que nos avise sobre que productos se han agregado al carrito va ser un problema interactuar con el y este componente.
